fd316ccee2e10512043b9f5d16acb57e36e38268
vutlhari-proj <baloyivutlhari16@gmail.com>
2025-07-22 22:02:42 +0200

    Add files via upload

---
+++ BasicFrameApp.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package basicframeapp;
+
+import za.ac.tut.ui.MyFirstFrame;
+
+/**
+ *
+ * @author vutlh
+ */
+public class BasicFrameApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        MyFirstFrame mff;
+        
+        mff = new MyFirstFrame();
+    }
+    
+}
